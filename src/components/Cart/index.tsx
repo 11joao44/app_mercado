14999319462ffc2e57787de45a15cef3004e6584
João Pedro <11joao44@gmail.com>
2023-08-25 17:25:39 -0400

    bug

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -3,12 +3,9 @@ import { List, Modal, Overlay } from './style';
 import { RootReducer } from '../../list';
 import { close } from '../../list/reducers/cart';
 import { Container } from '../../styles';
-import { useAPI } from '../../hooks/useAPI';
 import Product from '../Product';
-import { Produto } from '../../types';
 
 const Cart = () => {
-  const { data: produtos } = useAPI<Produto[]>('https://api-mercado-11joao44.vercel.app/api/produtos');
 
   const { isOpen } = useSelector((state: RootReducer) => state.cart);
   const dispatch = useDispatch();
