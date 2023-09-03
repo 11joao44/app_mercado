649cff7a9393306f05deea7c035aa5bb618893ef
João Pedro <11joao44@gmail.com>
2023-09-02 22:41:50 -0400

    removendo codigo sem uso

--- src/hooks/useAPI.ts
+++ src/hooks/useAPI.ts
@@ -16,23 +16,5 @@ export function useAPI() {
       })
   }, [])
 
-  const adicionarProduto = async (novoProduto: Product) => {
-    try {
-      const response = await axios.post<Product>(
-        'https://api-mercado.vercel.app/api/produtos',
-        novoProduto
-      )
-
-      if (response.status === 201) {
-        // Produto adicionado com sucesso
-        setItems((prevItems) => [...prevItems, response.data])
-      } else {
-        console.error('Erro ao adicionar o produto à API')
-      }
-    } catch (error) {
-      console.error('Erro de solicitação ao adicionar o produto:', error)
-    }
-  }
-
   return items
 }
