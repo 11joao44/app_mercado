25ab69a5f4d7001e4e4b9e5cd13ac248e7f27c9e
João Pedro <11joao44@gmail.com>
2023-09-06 22:28:32 -0400

    ajuste

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -26,7 +26,7 @@ const Header = () => {
 
       <AbasStyle onClick={openCartt}>
         <img src="https://img.icons8.com/external-flatart-icons-solid-flatarticons/ffffff/external-cart-traditional-marketing-flatart-icons-solid-flatarticons.png" />
-        <span>{addedToCart.length - 20}</span>
+        <span>{addedToCart.length}</span>
       </AbasStyle>
     </HeaderStyle>
   )
